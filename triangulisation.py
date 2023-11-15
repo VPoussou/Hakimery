98586215ec001117a2ec049397a09ac86281f79b
VPoussou <94170838+VPoussou@users.noreply.github.com>
2023-11-15 11:33:55 +0100

    Etape de dev
    En cours d'écriture pour les nombres premiers

---
+++ triangulisation.py
@@ -0,0 +1,26 @@
+
+# So we have 
+# Grandeur des côtés d'un triangle => Rectangle = ?
+# Nombre => Premier ?
+# (divisible uniquement par lui-même ou par 1)
+
+def triangulisation(longueur_cote_a: float, longueur_cote_b: float, longueur_cote_c: float) -> bool:
+
+    liste_de_cotes = [longueur_cote_a, longueur_cote_b, longueur_cote_c]
+    liste_de_cotes_triee = sorted(liste_de_cotes)
+    print(liste_de_cotes_triee)
+    liste_des_cathetes = [liste_de_cotes_triee[0], liste_de_cotes_triee[1]]
+    hypothenuse = liste_de_cotes_triee[2]
+
+    carre_de_lhypotenuse = hypothenuse**2
+    print(carre_de_lhypotenuse)
+    carre_du_cathete_a = liste_des_cathetes[0]**2
+    carre_du_cathete_b = liste_des_cathetes[1]**2
+    somme_des_carres_des_cathetes = carre_du_cathete_a + carre_du_cathete_b
+    if carre_de_lhypotenuse == somme_des_carres_des_cathetes:
+        return True
+    else:
+        return False
+    
+
+print(triangulisation(3,4,5))
